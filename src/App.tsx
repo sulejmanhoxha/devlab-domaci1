7496b849b853531f9caa8be1ef4250359dbebf13
sulejmanhoxha <sulejman.hoxha2001@hotmail.com>
2024-06-16 03:53:48 +0200

    add theme toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import "react-toastify/dist/ReactToastify.css";
 
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
-import GlobalContextProvider from "./context/GlobalContext";
+import GlobalContextProvider, {
+  useGlobalContext,
+} from "./context/GlobalContext";
 import ChangePasswordPage from "./pages/ChangePassword";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
@@ -13,6 +15,7 @@ const App = () => {
   return (
     <>
       <GlobalContextProvider>
+        <ThemeButton />
         <Routes>
           <Route
             path="/"
@@ -50,3 +53,17 @@ const App = () => {
 };
 
 export default App;
+
+function ThemeButton() {
+  const { toggleTheme } = useGlobalContext();
+  return (
+    <>
+      <button
+        onClick={toggleTheme}
+        className="absolute right-4 top-4 z-20 rounded-md bg-gray-200 p-2 text-gray-900 dark:bg-gray-700 dark:text-gray-100"
+      >
+        Toggle Theme
+      </button>
+    </>
+  );
+}
--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -29,6 +29,10 @@ type GlobalContext = {
 
   // logout function
   logout: () => void;
+
+  // theme
+  theme: string;
+  toggleTheme: () => void;
 };
 
 export const GlobalContext = createContext<GlobalContext | null>(null);
@@ -70,6 +74,27 @@ export default function GlobalContextProvider({
     removeRefreshToken();
     navigate("/login");
   };
+
+  // theme state
+  // const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useLocalStorage("theme", "dark", {
+    raw: true,
+  });
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.classList.remove(theme);
+    document.documentElement.classList.add(newTheme);
+  };
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.classList.add(theme);
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
 
   // use the refresh token to get an access token. wont run if
   // refresh token is empty
@@ -134,6 +159,10 @@ export default function GlobalContextProvider({
 
         // logout function
         logout,
+
+        // theme
+        theme,
+        toggleTheme,
       }}
     >
       {children}
